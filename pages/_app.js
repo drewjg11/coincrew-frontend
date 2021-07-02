505f7500ca3396744bd9c50d752214713934b0ab
John Drew <drewjg11@gmail.com>
2021-07-02 07:55:24 -0500

    App structure and essentials.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import App from "next/app";
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
 
+class MyApp extends App {
+
+  render () {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />
+  }
+
+}
 export default MyApp
